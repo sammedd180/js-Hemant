ff5013cd1c87fc997cc7f5d8225e7bb7a678cd0d
sammedd180 <131434970+sammedd180@users.noreply.github.com>
2026-02-19 07:14:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+ "use strict"; // treat all JS code as newer version
+
+ // alert(3 + 3) // we are using nodejs, not browser
+
+ console.log(3 + 3)
+
+// code readability should be high 
+
+console.log("Hemant")
+
+let name = "Hemant"
+let age = 22
+let isLoggedId = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqe
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);      // object
