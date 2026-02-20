c381143555a26dcd3dee266dc4170676e2a35fe2
HEMANT PATIL <131434970+sammedd180@users.noreply.github.com>
2026-02-20 06:15:02 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 //const bigNumber = 54136984651252623n
 
@@ -32,7 +32,40 @@ const myFunction = function(){
     console.log("hello world!");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 //Extra info
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//  ******************************************************************
+
+//   Stack (Primitive) ,    Heap (Non-Primitive)
+
+let myYoutubename = "hemantpatil.com"
+
+let anothername = myYoutubename
+anothername = "justcoding"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@gooogle.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hemant@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
+
+
