e1818f0fe8eb542d5fe0bf21e45bb37f00267d70
HEMANT PATIL <131434970+sammedd180@users.noreply.github.com>
2026-02-20 11:01:18 +0000

    dates  In Js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,4 +1,3 @@
-
 // Dates
 
 let myDate = new Date()
@@ -7,10 +6,11 @@ let myDate = new Date()
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date(2026, 0, 20)
+// let myCreatedDate = new Date(2026, 0, 20, 5, 3)
+// let myCreatedDate = new Date("2026-02-20")
+let myCreatedDate = new Date("02-20-2026")
+// console.log(myCreatedDate.toDateString());
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
@@ -20,9 +20,9 @@ let myTimeStamp = Date.now()
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-// console.log(newDate);
-// console.log(newDate.getMonth() + 1);
-// console.log(newDate.getDay());
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
